6fe58abca7ba28224282ab221388b667df07b445
mptste <124529697+mptste@users.noreply.github.com>
2023-10-11 12:24:55 +0200

    Update DatabaseConnection.java

--- src/databaseConnection/DatabaseConnection.java
+++ src/databaseConnection/DatabaseConnection.java
@@ -9,24 +9,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * This is a singleton class which is being used for making a database connection 
+ * This is a singleton class which is being used for making a database connection
+ *
  * @author pc
  */
 
 public class DatabaseConnection {
-    
+
     private static Connection con = null;
-     
-    static { 
-        
+
+    static {
+
         try {
-           con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gymdatabase2", "root","");
-        }
-        catch (SQLException e) {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gymdatabase2", "root", "");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
+
     public static Connection getConnection() {
         return con;
     }
